0694427f33db6ae6e24db51d0052898522a6a784
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-18 01:16:26 +0530

    card loading skeleton component

--- react-ch1/src/App.jsx
+++ react-ch1/src/App.jsx
@@ -1,15 +1,25 @@
+import { useState, useEffect } from "react";
 import Card from "./components/Card/Card";
 import Skeleton from "./components/Skeleton/Skeleton";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [])
+  
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">React Skeleton</h1>
+      <h1 className="text-3xl font-bold underline text-center">
+        React Skeleton
+      </h1>
       <div className="flex justify-center items-center h-screen">
-      <Card />
-      <Skeleton />
+        {loading ? <Skeleton /> : <Card />}
       </div>
-      
     </>
   );
 }
